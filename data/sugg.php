9dc490e17a8153bd9012fd0e35bc4e1dd78a9088
Frédéric Glorieux <frederic.glorieux@fictif.org>
2022-11-06 00:09:07 +0100

    Requête modèle clique

--- data/sugg.php
+++ data/sugg.php
@@ -24,22 +24,27 @@ if (!count($dico_terme)) {
     echo "<!-- $t, mot inconnu -->";
     return; // rien à chercher
 }
+$dico_terme = array_unique($dico_terme);
 $dico_terme = '(' . implode(', ', $dico_terme) . ')';
 
 
 // $src_forme = $row['forme'];
 
-
+$reltype_clique = 10;
 $sql = "
 SELECT *
 FROM dico_rel
 INNER JOIN dico_terme
     ON dico_rel.dico_terme = dico_terme.id
 WHERE
-    reltype = 3
-    AND dico_terme NOT IN $dico_terme
-    AND dico_entree IN (SELECT dico_entree FROM dico_rel WHERE reltype = 3 AND dico_terme IN $dico_terme)
-ORDER BY deforme, volume_annee
+    reltype = $reltype_clique
+    AND dico_terme NOT IN $dico_terme -- exclure l’entrée demandée
+    AND clique IN ( -- les mots liés par une clique
+        SELECT DISTINCT clique FROM dico_rel WHERE 
+        reltype = $reltype_clique 
+        AND dico_terme IN $dico_terme
+    )
+ORDER BY deforme, volume_annee, refimg
 
 ";
 
@@ -51,17 +56,20 @@ echo "<!--", number_format(microtime(true) - $starttime, 3), " s. -->\n";
 $qfilter = null;
 
 // filtre par cote
-// ne pas suggérer un mot qu’on ne trouverait pas dans le corpus
+// ne pas suggérer un mot qu’on ne trouverait pas comme vedette dans le corpus
 $reqPars = Medict::reqPars();
 if ($reqPars[Medict::DICO_TITRE]) {
-    $dico_titre = "AND dico_titre IN (" . implode(", ", $reqPars[Medict::DICO_TITRE]) . ")";
+    $reltype_orth = 1;
+    $reltype_term = 2;
+
+    $dico_titre = "dico_titre IN (" . implode(", ", $reqPars[Medict::DICO_TITRE]) . ")";
     $sql = "
 SELECT *
 FROM dico_rel
 WHERE
     dico_terme = ?
-    AND (reltype = 1 OR reltype = 4)
-    $dico_titre
+    AND (reltype = $reltype_orth OR reltype = $reltype_term)
+    AND $dico_titre
 LIMIT 1;
     ";
     echo "<!-- FILTRE $sql -->\n";
@@ -80,19 +88,24 @@ INNER JOIN dico_volume
 WHERE dico_entree.id = ?
 ";
 $qentree = Medict::$pdo->prepare($sql);
-$last = null;
+$last_terme = null;
+$last_refimg = null;
+// boucler sur toutes les relations de mots liés
+// en ordre alpha des formes, et chrono inverse
+// $last_terme permet de grouper les relations par forme
 while ($rel = $qrel->fetch(PDO::FETCH_ASSOC)) {
-    if ($last != $rel['id']) {
+    if ($last_terme != $rel['id']) {
 
-        if ($last !== null) {
+        if ($last_terme !== null) { // clore un bloc mot si ouvert
             echo "\n</details>";
             echo "\n&#10;";
             flush();
         }
+        // mot inconnu du corpus choisi, on passe
         if ($qfilter) {
             $qfilter->execute(array($rel['id']));
             if (!$qfilter->fetch()) {
-                $last = null;
+                $last_terme = null;
                 continue;
             }
         }
@@ -107,17 +120,24 @@ while ($rel = $qrel->fetch(PDO::FETCH_ASSOC)) {
     echo " <small>(". $sugg['score'], ")</small>";
     */
         echo"</a></summary>";
-        $last = $rel['id'];
+        $last_terme = $rel['id'];
+        $last_refimg = null;
     }
+    // ne pas répéter la même page
+    if ($last_refimg !== null && $last_refimg == $rel['refimg']) {
+        continue;
+    }
+    $last_refimg = $rel['refimg'];
     $qentree->execute(array($rel['dico_entree']));
     $entree = $qentree->fetch();
-    $entree['in'] = 'V. ' . $forme;
+    // si pas vedette, rappel de l’indice à trouver
+    if (!$rel['orth']) $entree['in'] = $forme;
     $entree['page'] = $rel['page'];
     $entree['refimg'] = $rel['refimg'];
     $entree['page2'] = null;
     echo "\n".Medict::entree($entree);    
 }
-if ($last) {
+if ($last_terme) {
     echo "\n</details>";
     echo "\n&#10;";
     flush();    
