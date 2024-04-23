33e001e41897c8c3a2e51ff2ec50c675a51051c4
Frédéric Glorieux <frederic.glorieux@fictif.org>
2024-04-23 11:19:33 +0200

    Commentaire et simplification

--- data/trad.php
+++ data/trad.php
@@ -92,6 +92,8 @@ while ($rel = $qrel->fetch(PDO::FETCH_ASSOC)) {
         // pb dans les données le volume n’existe pas
         continue;
     }
+    // si un mot dans une clique de traduction est la vedette de l'article éviter
+    // « Même » in Même, mêmes. Littré Robin 13e éd., 1873...
     if ($rel['orth']) {
         $entree['in'] = null;    
     }
@@ -109,50 +111,3 @@ if ($last) {
     flush();    
 }
 return;
-
-$sql = "SELECT * FROM dico_trad WHERE src_sort = ? ORDER BY dst_langno, dst_sort, volume_annee, page";
-$q_mot = Medict::$pdo->prepare($sql);
-$q_mot->execute(array($t));
-
-echo "<!-- $sql ; $t -->\n";
-
-$q_entree = Medict::$pdo->prepare("SELECT * FROM dico_entree WHERE id = ?");
-$last_sort = null;
-$last_lang = null;
-while ($row = $q_mot->fetch(PDO::FETCH_ASSOC)) {
-    if (
-        $last_lang != $row['dst_lang']
-        || $last_sort != $row['dst_sort']
-    ) {
-        $last_lang = $row['dst_lang'];
-        $last_sort = $row['dst_sort'];
-        // fermer le dernier
-        if ($last_sort !== null) {
-            echo "\n</details>";
-            echo "\n&#10;";
-            flush();
-        }
-        echo '
-<details class="sugg">
-    <summary title="' . $row['dst'] . '">
-      <a class="sugg" href="?q=' . rawurlencode($row['dst']) . '">'
-    . '<small>[' . $row['dst_lang'] . ']</small> ' . $row['dst'] . '</a></summary>';
-    }
-    $q_entree->execute(array($row['dico_entree']));
-    $entree = $q_entree->fetch();
-    if (!$entree) {
-        // pas normal, mais déjà vu
-        continue;
-    }
-
-    $entree['page'] = $row['page'];
-    $entree['page2'] = null;
-    $entree['refimg'] = $row['refimg'];
-
-
-    echo "\n".Medict::entree($entree);
-}
-echo "\n</details>";
-echo "\n&#10;";
-flush();
-echo '<!--', number_format(microtime(true) - $starttime, 3), ' s. -->';
